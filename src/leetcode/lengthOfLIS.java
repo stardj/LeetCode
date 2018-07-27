14ec06cc6bddffc40ef8766f16d0de0c1aab3fde
stardjyeah <stardjyeah@gmail.com>
2018-07-27 16:58:11 +0100

    update
    update

--- src/leetcode/lengthOfLIS.java
+++ src/leetcode/lengthOfLIS.java
@@ -1,5 +1,13 @@
 package leetcode;
 
+/*
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+Input: [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ */
+
 public class lengthOfLIS {
 
     public int lengthOfLIS(int[] nums) {
