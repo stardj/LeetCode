80dcca1ab59a31a95b35ef22dad33c676473f287
stardjyeah <stardjyeah@gmail.com>
2018-07-27 17:19:19 +0100

    update
    update

--- src/leetcode/lengthOfLIS.java
+++ src/leetcode/lengthOfLIS.java
@@ -8,6 +8,8 @@ Output: 4
 Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
  */
 
+import java.util.Arrays;
+
 public class lengthOfLIS {
 
     public int lengthOfLIS(int[] nums) {
@@ -26,13 +28,25 @@ public class lengthOfLIS {
         return result;
     }
 
-    public int lengthOfLIS2(int[] nums) {
+    public int[] lengthOfLIS2(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
 
+        for (int x : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            if (i < 0) i = -(i + 1);
+            dp[i] = x;
+            if (i == len) len++;
+        }
+        return dp;
     }
 
     public static void main(String[] args) {
         int[] tmp = {10, 9, 2, 5, 3, 7, 101, 18};
         lengthOfLIS lis = new lengthOfLIS();
-        System.out.println(lis.lengthOfLIS(tmp));
+//        System.out.println(lis.lengthOfLIS2(tmp));
+        for (int val : lis.lengthOfLIS2(tmp)) {
+            System.out.println(val);
+        }
     }
 }
