d26f58b6198e0b6a95446fd2ad23c688d8195372
stardjyeah <stardjyeah@gmail.com>
2018-07-28 20:25:28 +0100

    update
    update

--- src/leetcode/HouseRobberIII.java
+++ src/leetcode/HouseRobberIII.java
@@ -203,7 +203,19 @@ public class HouseRobberIII {
 
     //dfs solution
     public int rob(TreeNode root) {
-        return 0;
+        if (root == null) return 0;
+
+        int val = 0;
+
+        if (root.left != null) {
+            val += rob(root.left.left) + rob(root.left.right);
+        }
+
+        if (root.right != null) {
+            val += rob(root.right.left) + rob(root.right.right);
+        }
+
+        return Math.max(val + root.val, rob(root.left) + rob(root.right));
     }
 
 }
